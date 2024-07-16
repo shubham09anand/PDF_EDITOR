b1dea7e8c30f46c770a92bd82608c9cb883a7899
shubham09anand <shubhamsheelam@gmail.com>
2024-07-16 16:09:43 +0530

    init

--- pdf_editor/src/components/CreatePDF/SupportFiles/ContentSupport.jsx
+++ pdf_editor/src/components/CreatePDF/SupportFiles/ContentSupport.jsx
@@ -164,7 +164,7 @@ const ContentSupport = () => {
                                              <input id="website-url" type="text" className="bg-gray-50 border-r-0 border rounded-l-xl font-extrabold border-gray-300 text-gray-500 outline-none text-sm block w-full p-3" value={link.link} readOnly />
                                         </div>
                                         <div className="flex-shrink-0 z-10 outline-none inline-flex items-center py-3 px-4 text-sm font-medium text-center text-white cursor-pointer active:opacity-75 bg-gray-100 focus:outline-none border border-r-0" type="div">
-                                             <a href={link?.link} target='_blank'>
+                                             <a href={link?.link} target='_blank' rel='noreferrer'>
                                                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="w-5 h-5">
                                                        <path fillRule="evenodd" d="M19.902 4.098a3.75 3.75 0 0 0-5.304 0l-4.5 4.5a3.75 3.75 0 0 0 1.035 6.037.75.75 0 0 1-.646 1.353 5.25 5.25 0 0 1-1.449-8.45l4.5-4.5a5.25 5.25 0 1 1 7.424 7.424l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.757-1.757a3.75 3.75 0 0 0 0-5.304Zm-7.389 4.267a.75.75 0 0 1 1-.353 5.25 5.25 0 0 1 1.449 8.45l-4.5 4.5a5.25 5.25 0 1 1-7.424-7.424l1.757-1.757a.75.75 0 1 1 1.06 1.06l-1.757 1.757a3.75 3.75 0 1 0 5.304 5.304l4.5-4.5a3.75 3.75 0 0 0-1.035-6.037.75.75 0 0 1-.354-1Z" clipRule="evenodd" />
                                                   </svg>
