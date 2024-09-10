3393b8a26b9a67e0751477bbfdd73dfa5eab9112
shubham09anand <shubhamsheelam@gmail.com>
2024-09-10 15:31:17 +0530

    init

--- PDF_Editor_Front_End/src/Api/Api.js
+++ PDF_Editor_Front_End/src/Api/Api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: process.env.REACT_APP_API_URL__NETWORK });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL_NETWORK });
 
 API.interceptors.request.use((req) => {
   // You can add additional logic here, such as attaching tokens
