e64be6700276c5e9e2f72e2bf716aa41be139f64
shubham09anand <shubhamsheelam@gmail.com>
2025-05-14 16:53:30 +0530

    init

--- PDF_Editor_Front_End/src/Api/Api.js
+++ PDF_Editor_Front_End/src/Api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-// const API = axios.create({ baseURL: 'https://apipdfcollaborator.shubham09anand.in/auth' });
+const API = axios.create({ baseURL: 'https://apipdfcollaborator.shubham09anand.in/auth' });
 // const API = axios.create({ baseURL: 'http://13.202.210.238:8081/auth' });
-const API = axios.create({ baseURL: 'http://127.0.0.1:8080/auth' });
+// const API = axios.create({ baseURL: 'http://127.0.0.1:8080/auth' });
 
 API.interceptors.request.use((req) => {
   return req;
